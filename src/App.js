239fec7e920d8ad01938cbd038099654ec20fc4d
Brandon Tsui <itstsui@gmail.com>
2020-05-10 11:50:30 -0400

    auth0 for frontend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 // src/App.js
 
 import React from "react";
+import PrivateRoute from "./components/PrivateRoute";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -11,8 +12,13 @@ const NavBar = () => {
             {!isAuthenticated && (
                 <button onClick={() => loginWithRedirect({})}>Log in</button>
             )}
-
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>
+                    <Link to="/profile">Profile</Link>
+                </span>
+            )}
         </div>
     );
 };
