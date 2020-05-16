1699110e394497c6fdc83eb1e3a6eb079777da57
Brandon Tsui <itstsui@gmail.com>
2020-05-15 20:10:20 -0400

    added meta for google OAuth verification

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,6 +36,11 @@ const Home = () => {
                 }
                 {loaded &&
                     <>
+                        <h1 className="home-welcome">Welcome to Clackur!</h1>
+                        <div className="home-welcome-message">
+                            <div>Clackur is an image sharing app mechanical keyboard enthusiasts by keyboard enthusiasts.</div>
+                            <div>To demo, login with email: <strong>demo@demo.com</strong> password: <strong>aA1!demo</strong> or you can signup with any email or google account! </div>
+                        </div>
                         < div className="posts-container" >
                             {posts.map((post) => <SinglePost post={post} key={post.id} />)}
                         </div >
