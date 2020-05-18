a7bbaedf69d1edd1921588aead137f775cd31168
Brandon Tsui <itstsui@gmail.com>
2020-05-18 10:31:45 -0400

    updated welcome message

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,8 +36,8 @@ const Home = () => {
                     <>
                         <h1 className="home-welcome">Welcome to Clackur!</h1>
                         <div className="home-welcome-message">
-                            <div>Clackur is an image sharing app mechanical keyboard enthusiasts by keyboard enthusiasts.</div>
-                            <div>For experience all the features clackur has to offer, login, signup, or use the demo login!</div>
+                            <div>Clackur is an image sharing app for mechanical keyboard enthusiasts by keyboard enthusiasts.</div>
+                            <div>Experience all the features clackur has to offer by logging in, signing up, or using the demo login!</div>
                             <div>To demo, login with email: <strong>demo@demo.com</strong> password: <strong>aA1!demo</strong> or you can signup with any email or google account! </div>
                         </div>
                         < div className="posts-container" >
